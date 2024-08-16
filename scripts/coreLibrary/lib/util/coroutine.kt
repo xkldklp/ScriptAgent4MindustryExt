27bac77a2267a7cc6e53f4aed655ccfd9c8385e2
way-zer <himc.wicp@gmail.com>
2024-08-16 21:16:44 +0800

    :sparkles: (coreLib) 为`loop`函数增加try catch

--- scripts/coreLibrary/lib/util/coroutine.kt
+++ scripts/coreLibrary/lib/util/coroutine.kt
@@ -3,17 +3,30 @@ package coreLibrary.lib.util
 import cf.wayzer.scriptAgent.define.Script
 import cf.wayzer.scriptAgent.define.ScriptUtil
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.logging.Level
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.cancellation.CancellationException
 
-fun CoroutineScope.loop(context: CoroutineContext = EmptyCoroutineContext, block: suspend CoroutineScope.() -> Unit) {
+fun Script.loop(context: CoroutineContext = EmptyCoroutineContext, block: suspend CoroutineScope.() -> Unit) {
     launch(context) {
         while (true) {
-            block()
+            try {
+                block()
+            } catch (e: Exception) {
+                if (e is CancellationException) throw e
+                logger.log(Level.WARNING, "Exception inside loop, auto sleep 10s.", e)
+                delay(10000)
+            }
         }
     }
 }
+
+@Deprecated(level = DeprecationLevel.HIDDEN, message = "Only for script")
+fun CoroutineScope.loop(context: CoroutineContext = EmptyCoroutineContext, block: suspend CoroutineScope.() -> Unit) =
+    (this as Script).loop(context, block)
 
 @ScriptUtil
 inline fun <T> Script.withContextClassloader(loader: ClassLoader = javaClass.classLoader, block: () -> T): T {
