7af68df746dd66212027c1c0d0ea65fc8007bb45
way-zer <himc.wicp@gmail.com>
2024-09-03 22:17:25 +0800

    ⬆️ library 1.10.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,7 +85,7 @@ fun defineModule(
 }
 
 dependencies {
-    val libraryVersion = "1.10.6"
+    val libraryVersion = "1.10.6.1"
     val mindustryVersion = "ca40f700fb" //v146.004
     val pluginImplementation by configurations
     pluginImplementation("cf.wayzer:ScriptAgent:$libraryVersion")
